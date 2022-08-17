874f9b86d1ab650bccf8727fe01a6a83dd45615a
Eric TSOI Tsz Chun <61784292+horserosemilkshake@users.noreply.github.com>
2022-08-17 17:08:15 +0800

    Add files via upload

---
+++ src/components/Left.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Point from './Point';
+import Profile from './Profile.js';
+
+export default function Left() {
+    return (
+        <div>
+            <Profile></Profile>
+            <Point></Point>
+        </div>
+    )
+}
